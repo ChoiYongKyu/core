2a3475cee36b65296eaf6c8483593446ca2c02a0
cyk <chldydrb802@naver.com>
2022-07-19 23:54:28 +0900

    2022 07 19 ApplicationContextBasicFindTest

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -18,6 +18,7 @@ public class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("name = " + beanDefinitionName + " object " + bean);
+
         }
     }
 
@@ -31,7 +32,7 @@ public class ApplicationContextInfoTest {
 
 
             //Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
-            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는링빈
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = " + beanDefinitionName + " object " + bean);
